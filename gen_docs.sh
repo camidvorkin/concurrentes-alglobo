d2243bf2fe79a5b667d18e9afc6cdf9f2d093a7d
FdelMazo <fdelmazo@fi.uba.ar>
2021-10-25 10:49:28 -0300

    Fix img directory for gh pages

--- gen_docs.sh
+++ gen_docs.sh
@@ -6,4 +6,4 @@ cargo doc --no-deps --target-dir docs --open
 # We also remove every sequence of '//!' to account for line skips
 # We also rectify the img path to be relative to the root of the repo in the PDF, but not in the rustdoc
 # We also remove the dummy main method
-cat src/informe.rs  | sed 's/\/\/!\ //g' | sed 's/\/\/!//g' | sed 's/..\/..\/..\/img/img/g' | sed 's/fn main() {}//g' | pandoc -f markdown --metadata-file pandoc_headers.md -o docs/informe.pdf
+cat src/informe.rs  | sed 's/\/\/!\ //g' | sed 's/\/\/!//g' | sed 's/..\/..\/img/docs\/img/g' | sed 's/fn main() {}//g' | pandoc -f markdown --metadata-file pandoc_headers.md -o docs/informe.pdf
--- src/informe.rs
+++ src/informe.rs
@@ -97,7 +97,7 @@
 //!
 //! Una vez que se completa el request a la aerolínea, se procede a agregar las estadísticas correspondientes, se suma el tiempo total que tardó en procesar el pedido de principio a fin y se agrega la ruta solicitada(para agregar estas estadísticas, será necesario obtener el lock para poder leer el estado actual de las estadísticas y agregar las nuevas).
 //!
-//! ![Threads](../../../img/threads.jpg)
+//! ![Threads](../../img/threads.jpg)
 //!
 //! ## Segunda implementación -- Actores
 //!
